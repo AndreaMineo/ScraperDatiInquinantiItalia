79b3a10bed3337593714092ff0cdf7224e32a593
AndreaMineo <92448038+AndreaMineo@users.noreply.github.com>
2022-04-28 15:38:48 +0200

    Delete ScrapingDatiInquinantiComuniItalia directory

--- ScrapingDatiInquinantiComuniItalia/R/data.R
+++
@@ -1,9 +0,0 @@
-#'Dati comuni italiani forniti dall'ISTAT(2022)
-#'
-#' @format dataframe con 7904 righe e 3 variabili:
-#'  \describe{
-#'      \item{regione: denominazione regione di appartenenza del comune}
-#'      \item{provincia: denominazione provincia di appartenenza del comune}
-#'      \item{comune: denominazione comune}
-#   }
-"datiComuniItalia"
